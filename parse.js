7433165a4f4895ff5efa7300b102f3f2aa64832a
bbartolome <bbartolome@users.noreply.github.com>
2017-09-15 20:16:55 -0400

    Revert "fix: average wager"
    This reverts commit 789ba5fe80b03eebbdb0b4b199093d4e20b4b61b.

--- parse.js
+++ parse.js
@@ -11,71 +11,86 @@ var parser = require('csv-parse')();
 var results = {
 	eurusd: {
 		hold: {
-			wager: 0.0,
+			chance: 0.0,
+			odds: 0.0,
 			total: 0
 		},
 		long: {
-			wager: 0.0,
+			chance: 0.0,
+			odds: 0.0,
 			total: 0
 		},
 		short: {
-			wager: 0.0,
+			chance: 0.0,
+			odds: 0.0,
 			total: 0
 		}
 	},
 	usdchf: {
 		hold: {
-			wager: 0.0,
+			chance: 0.0,
+			odds: 0.0,
 			total: 0
 		},
 		long: {
-			wager: 0.0,
+			chance: 0.0,
+			odds: 0.0,
 			total: 0
 		},
 		short: {
-			wager: 0.0,
+			chance: 0.0,
+			odds: 0.0,
 			total: 0
 		}
 	},
 	usdjpy: {
 		hold: {
-			wager: 0.0,
+			chance: 0.0,
+			odds: 0.0,
 			total: 0
 		},
 		long: {
-			wager: 0.0,
+			chance: 0.0,
+			odds: 0.0,
 			total: 0
 		},
 		short: {
-			wager: 0.0,
+			chance: 0.0,
+			odds: 0.0,
 			total: 0
 		}
 	},
 	gbpusd: {
 		hold: {
-			wager: 0.0,
+			chance: 0.0,
+			odds: 0.0,
 			total: 0
 		},
 		long: {
-			wager: 0.0,
+			chance: 0.0,
+			odds: 0.0,
 			total: 0
 		},
 		short: {
-			wager: 0.0,
+			chance: 0.0,
+			odds: 0.0,
 			total: 0
 		}
 	},
 	audusd: {
 		hold: {
-			wager: 0.0,
+			chance: 0.0,
+			odds: 0.0,
 			total: 0
 		},
 		long: {
-			wager: 0.0,
+			chance: 0.0,
+			odds: 0.0,
 			total: 0
 		},
 		short: {
-			wager: 0.0,
+			chance: 0.0,
+			odds: 0.0,
 			total: 0
 		}
 	},
@@ -115,7 +130,6 @@ parser.on('readable', function(){
 	var record;
 	var chance;
 	var odds;
-	var wager;
 	var meetsCriterion;
 	var currency;
 	var position;
@@ -128,8 +142,8 @@ parser.on('readable', function(){
 
 		if(chance > 0.0 && odds > 0.0) {
 			if(meetsCriterion) {
-				wager = chance - ((1.0 - chance) / odds);
-				results[currency][position]['wager'] = Math.max(results[currency][position]['wager'], wager);
+				results[currency][position]['chance'] = (results[currency][position]['chance'] || 0.0) + chance;
+				results[currency][position]['odds'] = (results[currency][position]['odds'] || 0.0) + odds;
 
 				results[currency][position]['total'] = (results[currency][position]['total'] || 0) + 1.0;
 			}
@@ -145,6 +159,9 @@ parser.on('finish', function(){
 	var currencies = ['eurusd', 'usdchf', 'usdjpy','gbpusd', 'audusd', 'usdcad', 'nzdusd'];
 	var positions = ['hold', 'long', 'short'];
 	var currency, position;
+	var chance = 0.0;
+	var odds = 0.0;
+	var wager;
 	var totalWager;
 	var stopLoss;
 
@@ -156,8 +173,22 @@ parser.on('finish', function(){
 		for(var j=0; j < positions.length; j++) {
 			currency = currencies[i];
 			position = positions[j];
+			odds = 0.0;
 
-			console.log(position, ':', (results[currency][position]['wager'] * 100.0) + '%');
+			if(results[currency][position]['total']) {
+				chance = results[currency][position]['chance'] / results[currency][position]['total'];
+				odds = (results[currency][position]['odds'] / results[currency][position]['total']) - 1.0;
+			}
+
+			if(odds > 0.0) {
+				wager = chance - ((1.0 - chance) / odds);
+				results[currency][position]['wager'] = wager;
+
+				console.log(position, ':', (wager * 100.0) + '%');
+			} else {
+				wager = 0.0;
+				results[currency][position]['wager'] = 0.0;
+			}
 		}
 
 		totalWager =  results[currency]['long']['wager'] - results[currency]['short']['wager'];
