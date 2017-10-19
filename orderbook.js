90064e7e6de0def5ec843f0f12982f5757320b3b
bbartolome <bbartolome@users.noreply.github.com>
2017-10-19 18:13:43 -0400

    fix: orderbook.js

--- orderbook.js
+++ orderbook.js
@@ -1,59 +1,116 @@
 var nconf = require('nconf');
 nconf.argv();
 
-var request = require('request');
-var requestUrl = 'https://api-fxpractice.oanda.com/labs/v1/orderbook_data?' +
-	'instrument=' + nconf.get('instrument') + '&' +
-	'period=' + nconf.get('period');
-
 var _ = require('lodash');
+var request = require('request');
 
-var requestOpts = {
-	url: requestUrl,
-	headers: {
-		'Authorization': 'Bearer ' + nconf.get('token')
-	}
-};
+function fetchResults(instrument, period, margin, done) {
+	var requestUrl = 'https://api-fxpractice.oanda.com/labs/v1/orderbook_data?' +
+		'instrument=' + instrument + '&' +
+		'period=' + period;
 
-request(requestOpts, function(error, response, body) {
-	var data = JSON.parse(body);
+	var requestOpts = {
+		url: requestUrl,
+		headers: {
+			'Authorization': 'Bearer ' + nconf.get('token')
+		}
+	};
 
-	_.each(_.sortBy(_.keys(data)), function(timestamp) {
-		var rate = data[timestamp].rate;
-		var sum = { bid: 0.0, ask: 0.0 };
-		var total = { bid: 0, ask: 0 };
-		var max = { os: 0.0, ask: 0.0 };
-		var min = { ol: 0.0, bid: 0.0 };
+	var results = {};
 
-		_.each(_.reverse(_.sortBy(_.keys(data[timestamp].price_points))), function(pricePoint) {
-			var net = (data[timestamp].price_points[pricePoint].ol - data[timestamp].price_points[pricePoint].os);
-			var distOrig = pricePoint - rate;
-			var dist = Math.abs(distOrig);
-			var margin = parseFloat(nconf.get('margin') || 1.0);
+	request(requestOpts, function(error, response, body) {
+		var data = JSON.parse(body);
 
-			if(dist < margin * rate) {
-				if(distOrig < 0.0) {
-					if(net > min.ol) {
-						min.ol = net;
-						min.bid = rate - dist;
+		_.each(_.reverse(_.sortBy(_.keys(data))), function(timestamp) {
+			var rate = data[timestamp].rate;
+			var sum = { bid: 0.0, ask: 0.0 };
+			var total = { bid: 0, ask: 0 };
+			var max = { os: 0.0, ask: 0.0 };
+			var min = { ol: 0.0, bid: 0.0 };
+
+			_.each(_.sortBy(_.keys(data[timestamp].price_points)), function(pricePoint) {
+				var net = (data[timestamp].price_points[pricePoint].ol - data[timestamp].price_points[pricePoint].os);
+				var distOrig = pricePoint - rate;
+				var dist = Math.abs(distOrig);
+
+				if(dist < margin * rate) {
+					if(distOrig < 0.0) {
+						if(net > min.ol) {
+							min.ol = net;
+							min.bid = rate - dist;
+						}
+						sum.bid = sum.bid + (net * dist);
+						total.bid++;
+					} else {
+						if(net < max.os) {
+							max.os = net;
+							max.ask = rate + dist;
+						}
+						sum.ask = sum.ask + (net * dist);
+						total.ask++;
 					}
-					sum.bid = sum.bid + (net * dist);
-					total.bid++;
-				} else {
-					if(net < max.os) {
-						max.os = net;
-						max.ask = rate + dist;
-					}
-					sum.ask = sum.ask + (net * dist);
-					total.ask++;
 				}
-			}
+			});
+
+			var bid = rate - Math.abs(sum.bid / total.bid);
+			var ask = rate + Math.abs(sum.ask / total.ask);
+			var average = (bid + ask) / 2.0;
+			var bullish = (sum.bid / (Math.abs(sum.bid) + Math.abs(sum.ask))) > 0.5;
+
+			results[timestamp] = {
+				rate: rate,
+				max: max.ask,
+				ask: ask,
+				bid: bid,
+				min: min.bid,
+				average: average,
+				bullish: bullish
+			};
 		});
 
-		var bid = rate - Math.abs(sum.bid / total.bid);
-		var ask = rate + Math.abs(sum.ask / total.ask);
-		var average = (bid + ask) / 2.0;
-
-		console.log(new Date(timestamp * 1000), rate, bid, ask, average, (sum.bid / (Math.abs(sum.bid) + Math.abs(sum.ask))) > 0.5 ? 'BUY:' + max.ask : 'SELL:' + min.bid);
+		done(null, results);
 	});
-});
+}
+
+function main() {
+	var instrument = nconf.get('instrument');
+	var period;
+	var margin = parseFloat(nconf.get('margin')) || 1.0;
+
+	switch(nconf.get('period')) {
+		case 'year':
+			period = 31536000;
+			break;
+		case 'month':
+			period = 2592000;
+			break;
+		case 'week':
+			period = 604800;
+			break;
+		case 'day':
+			period = 86400;
+			break;
+		default:
+			period = 3600;
+			break
+	}
+
+	switch(instrument) {
+		default:
+			fetchResults(instrument, period, margin, function handleResults(err, results) {
+				_.each(_.keys(results), function (timestamp) {
+					var latest = results[timestamp];
+
+					console.log('==========');
+					console.log('DATE', '-', new Date(timestamp * 1000));
+					console.log('SELL - ' + JSON.stringify({ sl: latest.max, limit: latest.ask, tp: latest.bid, min: latest.min }));
+					console.log('BUY - ' + JSON.stringify({ sl: latest.min, limit: latest.bid, tp: latest.ask, max: latest.max }));
+					console.log('----------');
+					console.log(latest.bullish ? 'UP' : 'DOWN', '-', 'avg:' + latest.average, latest.bullish ? '<' : '>', 'rate:' + latest.rate);
+					console.log();
+				});
+			});
+	}
+}
+
+main();
